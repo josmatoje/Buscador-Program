8d6354daa89d1fb6832215c2f4034ea12267ed68
josmatoje <josemariamataojeda@gmail.com>
2021-02-24 12:55:23 +0100

    metodo duplicar Array

--- src/Utilidades/Utilidad.java
+++ src/Utilidades/Utilidad.java
@@ -5,6 +5,8 @@ public class Utilidad {
     public static String[] aumentarArray (String[] arrayLleno){
 
         String[] arrayDoble = new String [arrayLleno.length*2];
+        for(int i=0; i<arrayLleno.length; i++)
+            arrayDoble[i]=arrayLleno[i];
 
         return arrayDoble;
     }
