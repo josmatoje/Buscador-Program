11851b30ae6b9f9149c646d45779da2a584bcc1a
Pedro <krazyabyss@gmail.com>
2021-03-06 19:18:37 +0100

    Clases tests creadas, eliminada la primaria

--- src/tests/TestValidacion.java
+++ src/tests/TestValidacion.java
@@ -1,5 +1,56 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clasesBasicas.Pagina;
+import validaciones.Validacion;
+
 public class TestValidacion {
+	static Pagina paginaDePrueba;
+	static Pagina paginaMala;
+	static Pagina[] paginas = new Pagina[2];
+	static Validacion validacion = new Validacion();
+	
+	
+	@BeforeAll
+
+	/*
+	 * Objetos necesarios para los test que se crearan antes de la ejecucion de
+	 * estos
+	 */
+	static void Pagina() {
+		paginaDePrueba = new Pagina("https://ciclo.iesnervion.es", "pagina hecha para probar los enlaces buenos",
+				new String[] { "informatica" }, "");
+		paginaMala = new Pagina("enlaceMalo.com", "pagina hecha para probar los enlaces malos",
+				new String[] { "buscador" }, paginaDePrueba.getUrl());
+		paginas[0] = paginaDePrueba;
+		paginas[1] = paginaMala;
+	}
+	
+	
+	/**
+	 * Descripcion: Test para comprobar que la validacion de url funciona bien
+	 * cuando hay una url valida Metodo a testear: validarUrl(String url) de la
+	 * clase Comprobaciones.
+	 */
+	@Test
+	void testUrlValida() {
+		assertTrue(Validacion.validarUrl(paginaDePrueba.getUrl()));
+	}
+
+	
+	/**
+	 * Descripcion: Test para comprobar que la validacion de url funciona bien
+	 * cuando se mete una url no valida. Metodo a testear: validarUrl(String url) de
+	 * la clase Comprobaciones.
+	 */
+	@Test
+	void testUrlNoValida() {
+		assertFalse(Validacion.validarUrl(paginaMala.getUrl()));
+	}
 
 }
--- src/tests/TestsUtilidad.java
+++ src/tests/TestsUtilidad.java
@@ -1,5 +1,49 @@
 package tests;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import clasesBasicas.Pagina;
+import gestion.Utilidad;
+import validaciones.Validacion;
+
 public class TestsUtilidad {
+	static Pagina paginaDePrueba;
+	static Pagina paginaMala;
+	static Pagina[] paginas = new Pagina[2];
+	static Validacion validacion = new Validacion();
+	
+	
+	@BeforeAll
+
+	/*
+	 * Objetos necesarios para los test que se crearan antes de la ejecucion de
+	 * estos
+	 */
+	static void Pagina() {
+		paginaDePrueba = new Pagina("https://ciclo.iesnervion.es", "pagina hecha para probar los enlaces buenos",
+				new String[] { "informatica" }, "");
+		paginaMala = new Pagina("enlaceMalo.com", "pagina hecha para probar los enlaces malos",
+				new String[] { "buscador" }, paginaDePrueba.getUrl());
+		paginas[0] = paginaDePrueba;
+		paginas[1] = paginaMala;
+	}
+
+	
+	/**
+	 * Descripcion: Test para comprobar que el metodo aumentar array funciona
+	 * correctamente Metodo a testear: aumentarArray(Object [] arrayLleno)
+	 */
+
+	@Test
+	void aumentarArray() {
+		paginas = Utilidad.aumentarArray(paginas);
+		paginas = Utilidad.aumentarArray(paginas);
+		paginas = Utilidad.aumentarArray(paginas);
+		paginas = Utilidad.aumentarArray(paginas);
+		assertEquals(32, paginas.length);
+	}
 
 }
