d89f6efc2148860fd81e1f83eb58d295942b2fec
Santi <santima1904@gmail.com>
2021-03-07 18:43:07 +0100

    Auto stash before merge of "Santi" and "origin/Pedrito"

--- src/tests/TestsUtilidad.java
+++ src/tests/TestsUtilidad.java
@@ -51,5 +51,52 @@ public class TestsUtilidad {
 		paginas = Utilidad.aumentarArray(paginas);
 		assertEquals(32, paginas.length);
 	}
-}
+
+	//Tests para metodo palabras coincidentes
 	
+	/**
+	 * Descripcion: Test para probar que hay palabras coincidentes
+	 */
+	
+	@Test
+	void palabrasCoincidentesCoinciden() {
+		assertEquals(3, Utilidad.palabrasCoincidentes(paginas[0].getPalabrasClaves(), paginas[1].getPalabrasClaves()));
+	}
+	
+	/**
+	 * Descripcion: Test para probar que no hay palabras coincidentes
+	 */
+	
+	@Test
+	void palabrasCoincidentesNoCoinciden() {
+		 String [] palabrasNoCoincidentes = new String[3];//array de palabras diferentes al creado anteriormente
+		
+		 palabrasNoCoincidentes[0] = "hola";
+		 palabrasNoCoincidentes[1] = "adios";
+		 palabrasNoCoincidentes[2] = "aguacate";
+		 
+		
+		assertEquals(0, Utilidad.palabrasCoincidentes(paginas[0].getPalabrasClaves(), palabrasNoCoincidentes));
+	}
+	
+	/**
+	 * Descripcion: Test para probar que no el array para comprobar no tiene palabras clave
+	 */
+	
+	@Test
+	void palabrasCoincidentesNoHayPalabrasClavePalabrasComprobar() {
+		
+		assertEquals(0, Utilidad.palabrasCoincidentes(paginas[0].getPalabrasClaves(), arrayVacia));
+	}
+	
+	/**
+	 * Descripcion: Test para probar que no el array de la pagina no tiene palabras clave
+	 */
+	
+	@Test
+	void palabrasCoincidentesNoHayPalabrasClavePalabrasPagina() {
+		
+		
+		assertEquals(0, Utilidad.palabrasCoincidentes(arrayVacia, arrayVacia));
+	}
+}
